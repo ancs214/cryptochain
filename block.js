2ce1d6cdc6d7f702cbca3f7322947c03c3d27f61
ancs214 <ancs214@gmail.com>
2022-08-18 18:51:19 -0400

    added nonce value and difficulty criteria to block class and completed testing

--- block.js
+++ block.js
@@ -1,35 +1,48 @@
-const { GENESIS_DATA } = require("./config");
-const cryptoHash = require("./crypto-hash");
+const { GENESIS_DATA } = require('./config')
+const cryptoHash = require('./crypto-hash')
 
 //create Block class
 class Block {
-    //arguments wrapped in an object so we dont have to know the order later on
-    constructor({ timestamp, lastHash, hash, data }) {
-        //set a property on an instance of the class
-        this.timestamp = timestamp;
-        this.lastHash = lastHash;
-        this.hash = hash;
-        this.data = data;
-    }
+  //arguments wrapped in an object so we dont have to know the order later on
+  constructor({ timestamp, lastHash, hash, data, nonce, difficulty }) {
+    //set a property on an instance of the class
+    this.timestamp = timestamp
+    this.lastHash = lastHash
+    this.hash = hash
+    this.data = data
+    this.nonce = nonce
+    this.difficulty = difficulty
+  }
 
-//first block - to get blockchain going
-static genesis() {
-    return new this(GENESIS_DATA);
-    }
+  //first block - to get blockchain going
+  static genesis() {
+    return new this(GENESIS_DATA)
+  }
 
-//new block
-static mineBlock({ lastBlock, data }) {
-    const timestamp = Date.now();
-    const lastHash = lastBlock.hash;
+  //new block
+  static mineBlock({ lastBlock, data }) {
+    let hash, timestamp
+    const lastHash = lastBlock.hash
+    //destructure difficulty value from lastBlock
+    const { difficulty } = lastBlock
+    let nonce = 0
+
+    //while hash.substring does not have the correct difficulty criteria, increase nonce by one and run again
+    do {
+      nonce++
+      timestamp = Date.now()
+      hash = cryptoHash(timestamp, lastHash, data, nonce, difficulty)
+    } while (hash.substring(0, difficulty) !== '0'.repeat(difficulty))
 
     return new this({
-        timestamp,
-        lastHash,
-        data,
-        hash: cryptoHash(timestamp, lastHash, data)
-    });
-}
+      timestamp,
+      lastHash,
+      data,
+      difficulty,
+      nonce,
+      hash,
+    })
+  }
 }
 
-module.exports = Block;
-
+module.exports = Block
--- config.js
+++ config.js
@@ -1,9 +1,13 @@
+const INITIAL_DIFFICULTY = 3
+
 //hardcoded and global values
 const GENESIS_DATA = {
-    timestamp: 1,
-    lastHash: '------',
-    hash: 'hash-one',
-    data: []
-};
+  timestamp: 1,
+  lastHash: '------',
+  hash: 'hash-one',
+  difficulty: INITIAL_DIFFICULTY,
+  nonce: 0,
+  data: [],
+}
 
-module.exports = { GENESIS_DATA };
+module.exports = { GENESIS_DATA }
