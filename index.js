1d77cf2437be656613d5f94bfb91153aa4e5a04d
ancs214 <ancs214@gmail.com>
2022-08-18 22:30:21 -0400

    added request npm to sync chains on request so each server instance will have an updated blockchain

--- index.js
+++ index.js
@@ -1,11 +1,17 @@
 const bodyParser = require('body-parser')
 const express = require('express')
+//using to sync chain on request
+const request = require('request')
 const Blockchain = require('./blockchain')
 const PubSub = require('./pubsub')
 
 const app = express()
 const blockchain = new Blockchain()
 const pubsub = new PubSub({ blockchain })
+
+//for use with 'request' to sync chain
+const DEFAULT_PORT = 3000
+const ROOT_NODE_ADDRESS = `http://localhost:${DEFAULT_PORT}`
 
 setTimeout(() => pubsub.broadcastChain(), 1000)
 
@@ -29,7 +35,21 @@ app.post('/api/mine', (req, res) => {
   res.redirect('/api/blocks')
 })
 
-const DEFAULT_PORT = 3000
+//use 'request' to sync chains
+const syncChains = () => {
+  request(
+    { url: `${ROOT_NODE_ADDRESS}/api/blocks` },
+    (error, response, body) => {
+      if (!error && response.statusCode === 200) {
+        const rootChain = JSON.parse(body)
+
+        console.log('replace chain on a sync with', rootChain)
+        blockchain.replaceChain(rootChain)
+      }
+    }
+  )
+}
+
 let PEER_PORT
 
 //if process env running, define PEER_PORT as below
@@ -39,4 +59,8 @@ if (process.env.GENERATE_PEER_PORT === 'true') {
 
 //if PEER_PORT defined, PORT=PEER PORT, else PORT=DEFAULT PORT
 const PORT = PEER_PORT || DEFAULT_PORT
-app.listen(PORT, () => console.log(`Listening at localhost: ${PORT}`))
+app.listen(PORT, () => {
+  console.log(`Listening at localhost: ${PORT}`)
+
+  syncChains()
+})
