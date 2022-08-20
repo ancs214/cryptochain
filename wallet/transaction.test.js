d7a00ee2aca6b5fd6e25013401f8c97d02f16cd0
ancs214 <ancs214@gmail.com>
2022-08-20 12:40:11 -0400

    add transaction input property with timestamp, amt, address, and signature

--- wallet/transaction.test.js
+++ wallet/transaction.test.js
@@ -1,3 +1,4 @@
+const { verifySignature } = require('../util')
 const Wallet = require('./index')
 const Transaction = require('./transaction')
 
@@ -28,6 +29,34 @@ describe('Transaction', () => {
       expect(transaction.outputMap[senderWallet.publicKey]).toEqual(
         senderWallet.balance - amount
       )
+    })
+  })
+
+  describe('input', () => {
+    it('has an `input`', () => {
+      expect(transaction).toHaveProperty('input')
+    })
+
+    it('has a `timestamp` in the input', () => {
+      expect(transaction.input).toHaveProperty('timestamp')
+    })
+
+    it('sets the `amount` to the `senderWallet` balance', () => {
+      expect(transaction.input.amount).toEqual(senderWallet.balance)
+    })
+
+    it('sets the `address` to the `senderWallet` publicKey', () => {
+      expect(transaction.input.address).toEqual(senderWallet.publicKey)
+    })
+
+    it('signs the input', () => {
+      expect(
+        verifySignature({
+          publicKey: senderWallet.publicKey,
+          data: transaction.outputMap,
+          signature: transaction.input.signature,
+        })
+      ).toBe(true)
     })
   })
 })
